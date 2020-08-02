e88ce07b4e44279c036b488162bf20feeab45d88
Victor <victor.tran@tempestas.io>
2020-08-02 04:01:53 -0400

    finish

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,20 @@
-def word_count(s):
+import re
+
+def word_count(s: str):
     # Your code here
+    cache = {}
+    for char in '":;,.-+=/\\|[]{}()*^&':
+        s = s.replace(char, '')
+
+    s = re.sub(r'\s+', ' ', s).lower().split(' ')
+    for word in s:
+        try:
+            cache[word] += 1
+        except KeyError:
+            if not word:
+                continue
+            cache[word] = 1
+    return cache
 
 
 
@@ -8,3 +23,4 @@ if __name__ == "__main__":
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('a a\ra\na\ta \t\r\n'))
