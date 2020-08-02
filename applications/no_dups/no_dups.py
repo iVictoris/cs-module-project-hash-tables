5dec7248a7ad557eb10b123e640edcd767af40db
Victor <victor.tran@tempestas.io>
2020-08-02 04:11:43 -0400

    complete

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,5 +1,25 @@
 def no_dups(s):
     # Your code here
+    # order matters so I can't do this ' '.join(set(s.split())) 
+    order = {}
+    s = s.split(' ')
+    count = 0
+    for word in s:
+        try:
+            order[word]
+        except KeyError:
+            order[word] = count
+            count += 1
+
+    words = [None] * len(order)
+    for key in order:
+        words[order[key]] = key
+
+    return ' '.join(words)
+
+
+    # now go through order and look for value with 0, then 1, etc
+
 
 
 
